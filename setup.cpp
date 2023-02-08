13a87ea2a1e1259ce4e2f171e8b685c12f3cd410
Alejandro Acedo Ralston <124578233+AlexRalston@users.noreply.github.com>
2023-02-08 22:53:29 +0100

    Update setup.cpp

--- setup.cpp
+++ setup.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Actualizar la lista de paquetes
+    cout << "Actualizando la lista de paquetes...\n";
+    system("sudo apt-get update");
+
+    // Actualizar los paquetes instalados
+    cout << "Actualizando los paquetes instalados...\n";
+    system("sudo apt-get upgrade");
+
+    // Instalar Wireshark
+    cout << "Instalando Wireshark...\n";
+    system("sudo apt-get install wireshark");
+
+    // Instalar tcpdump
+    cout << "Instalando tcpdump...\n";
+    system("sudo apt-get install tcpdump");
+
+    cout << "El script ha finalizado correctamente.\n";
+    return 0;
+}
